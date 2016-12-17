6be2ced5aeef4c3d492410a32f1d935f97a350bc
Justin Lafferty <Justin Lafferty>
2016-12-17 12:44:17 -0600

    Removed chart.html.  Data is being served from express.

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var app = express();
-var path = require('path');
+
+//Express partials is needed so that a layout.ejs file can be used.
+var partials = require('express-partials');
+
+//Set the templating engine to embedded javascript and enable partials.
+app.set('view engine', 'ejs');
+app.use(partials());
 
 app.get('/', function(req, res) {
-    //res.send('Hello World!');
-    res.sendFile(path.join(__dirname + '/chart.html'));
+    //Send our fake data back to the client.
+    res.render('index', { data: '[12, 19, 3, 5, 2, 3]'});
 });
 
 app.listen(3000, function() {
