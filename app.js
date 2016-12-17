577e9785dafca32063b568fa768c5d652ead0e3f
Justin Lafferty <Justin Lafferty>
2016-12-17 17:36:22 -0600

    New dependencies, and random colors coming from the server.

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+var tools = require('./tools');
+
 var express = require('express');
 var app = express();
 
 //Express partials is needed so that a layout.ejs file can be used.
 var partials = require('express-partials');
+
+//This information will come from the database and
+//need to be put into array.  Might as well get started now.
+var dataArray = [];
+dataArray.push(12);
+dataArray.push(19);
+dataArray.push(3);
+dataArray.push(5);
+dataArray.push(2);
+dataArray.push(3);
 
 var labelArray = [];
 labelArray.push("Red");
@@ -17,9 +29,21 @@ app.set('view engine', 'ejs');
 app.use(partials());
 
 app.get('/', function(req, res) {
+
+    var fillColorArray = [];
+    var borderColorArray = [];
+
+    for(var i = 0; i < 6; ++i) {
+        var color = tools.randomColor();
+        fillColorArray.push(color[0]);
+        borderColorArray.push(color[1]);
+    }
+
     //Send our fake data back to the client.
-    res.render('index', { data: [12, 19, 3, 5, 2, 3],
-                          labels: labelArray});
+    res.render('index', { data: dataArray,
+                          labels: labelArray,
+                          fillColors: fillColorArray,
+                          borderColors: borderColorArray});
 });
 
 app.listen(3000, function() {
---
+++ tools.js
@@ -0,0 +1,28 @@
+
+//Needed so that the numbers are always random
+//and not just when script starts.
+var seedrandom = require('seedrandom');
+
+module.exports = {
+    randomColor: function(alphaChannel) {
+        var date = new Date();
+        var rng = seedrandom(date.getTime(), {entropy: true});
+        
+        var red = 0;
+        var green = 0;
+        var blue = 0;
+        var alpha = alphaChannel;
+
+        var fillColor = '';
+        var borderColor = '';
+
+        red = Math.floor(rng() * 255);
+        green = Math.floor(rng() * 255);
+        blue = Math.floor(rng() * 255);
+
+        fillColor = 'rgba(' + red + ',' + green + ',' + blue + ',' + '0.2)';
+        borderColor = 'rgba(' + red + ',' + green + ',' + blue + ',' + '1)';
+
+        return [fillColor, borderColor];
+    }
+};
