407b04692ce08e38bedf5aaf9515ee3ed46ab67e
Justin Lafferty <Justin Lafferty>
2016-12-17 16:58:55 -0600

    Labels are now coming from the server.

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ var app = express();
 //Express partials is needed so that a layout.ejs file can be used.
 var partials = require('express-partials');
 
+var labelArray = [];
+labelArray.push("Red");
+labelArray.push("Blue");
+labelArray.push("Yellow");
+labelArray.push("Green");
+labelArray.push("Purple");
+labelArray.push("Orange");
+
 //Set the templating engine to embedded javascript and enable partials.
 app.set('view engine', 'ejs');
 app.use(partials());
 
 app.get('/', function(req, res) {
     //Send our fake data back to the client.
-    res.render('index', { data: '[12, 19, 3, 5, 2, 3]'});
+    res.render('index', { data: [12, 19, 3, 5, 2, 3],
+                          labels: labelArray});
 });
 
 app.listen(3000, function() {
