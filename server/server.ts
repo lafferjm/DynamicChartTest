de771247230f6a21b8507d26e682380f2de95af7
Justin Lafferty <Justin Lafferty>
2016-12-18 11:30:45 -0600

    Minor code style changes.

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,5 @@
-import * as express from 'express'
-import expressPartial = require('express-partials')
+import * as express from 'express';
+import expressPartial = require('express-partials');
 
 export class ServerApp {
 
--- server/tools.ts
+++ server/tools.ts
@@ -1,4 +1,4 @@
-import * as seedrandom from 'seedrandom'
+import * as seedrandom from 'seedrandom';
 
 export class Tools {
     constructor() {
