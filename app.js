731cb9064ff0b5ad0fdd1da7b028439c92f09a7f
Justin Lafferty <Justin Lafferty>
2016-12-17 18:57:56 -0600

    Cleaning up.

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ var partials = require('express-partials');
 var sqlite3 = require('sqlite3').verbose();
 var database = new sqlite3.Database('test.db');
 
-var needsSetup = true;
-
 database.serialize(function() {
     database.run("CREATE TABLE IF NOT EXISTS colors (" +
                  "color TEXT NOT NULL," +
@@ -21,42 +19,22 @@ database.serialize(function() {
 database.all("SELECT COUNT(*) FROM colors;", function(error, rows) {
     var count = rows[0]["COUNT(*)"];
 
-    if(count > 0) {
-        needsSetup = false;
+    if(count == 0) {
+        var statement = database.prepare("INSERT INTO colors VALUES (?, ?)");
+        statement.run("Red", 12);
+        statement.run("Blue", 19);
+        statement.run("Yellow", 3);
+        statement.run("Green", 5);
+        statement.run("Purple", 2);
+        statement.run("Orange", 3);
     }
     
+    console.log("Count: " + count);
+
     if(error) {
         console.log("Errors: " + error);
     }
 });
-
-if(needsSetup) {
-    var statement = database.prepare("INSERT INTO colors VALUES (?, ?)");
-    statement.run("Red", 12);
-    statement.run("Blue", 19);
-    statement.run("Yellow", 3);
-    statement.run("Green", 5);
-    statement.run("Purple", 2);
-    statement.run("Orange", 3);
-}
-
-//This information will come from the database and
-//need to be put into array.  Might as well get started now.
-var dataArray = [];
-dataArray.push(12);
-dataArray.push(19);
-dataArray.push(3);
-dataArray.push(5);
-dataArray.push(2);
-dataArray.push(3);
-
-var labelArray = [];
-labelArray.push("Red");
-labelArray.push("Blue");
-labelArray.push("Yellow");
-labelArray.push("Green");
-labelArray.push("Purple");
-labelArray.push("Orange");
 
 //Set the templating engine to embedded javascript and enable partials.
 app.set('view engine', 'ejs');
@@ -64,18 +42,33 @@ app.use(partials());
 
 app.get('/', function(req, res) {
 
+    //Will contain the values from the database containing color
+    //and count
+    var colorArray = [];
+    var countArray = [];
+    
+    //Will contain random values for drawing the bars
     var fillColorArray = [];
     var borderColorArray = [];
-
-    for(var i = 0; i < 6; ++i) {
+    
+    database.each("SELECT color, count FROM colors;", function(error, rows) {
         var color = tools.randomColor();
+
+        colorArray.push(rows.color);
+        countArray.push(rows.count);
+
         fillColorArray.push(color[0]);
         borderColorArray.push(color[1]);
-    }
 
+        if(error) {
+            console.log(error);
+        }
+
+    });
+    
     //Send our fake data back to the client.
-    res.render('index', { data: dataArray,
-                          labels: labelArray,
+    res.render('index', { labels: colorArray,
+                          data: countArray,
                           fillColors: fillColorArray,
                           borderColors: borderColorArray});
 });
