e1ecaeab04cceb596fc8156daf7266f82e862ef7
Justin Lafferty <Justin Lafferty>
2016-12-18 19:00:38 -0600

    Trying lokijs, so added dependencies.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,5 @@
 "use strict";
-require("reflect-metadata");
 var server_1 = require("./server");
-var data_1 = require("./data");
 var serverApp = new server_1.ServerApp();
-var database = new data_1.Data();
 serverApp.setRoutes();
 serverApp.startServer();
