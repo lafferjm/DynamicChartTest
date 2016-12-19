7db46c17de11919592c5c70013f1b49faeca260f
Justin Lafferty <Justin Lafferty>
2016-12-18 18:40:15 -0600

    TypeORM was a bust, reverting changes.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 "use strict";
+require("reflect-metadata");
 var server_1 = require("./server");
+var data_1 = require("./data");
 var serverApp = new server_1.ServerApp();
+var database = new data_1.Data();
 serverApp.setRoutes();
 serverApp.startServer();
